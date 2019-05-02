b63703399903ee246d0254377c0c753cb787202c
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2019-05-02 17:15:07 +0100

    attah's edit

--- connect.py
+++ connect.py
@@ -1,24 +1,26 @@
 import pymysql
 
-connection = pymysql.connect(host = 'localhost', user = 'root', password = '', db = 'Scraper', charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor )
+connection = pymysql.connect(host = 'localhost', user = 'root', password = '', db = 'db', charset = 'utf8mb4', cursorclass = pymysql.cursors.DictCursor )
+
+table_name = "user" #table name here
 
 def create_table():
 
     with connection.cursor() as cursor:
 
         try:
-                sql = """ CREATE TABLE User(id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(100), emailAddress VARCHAR(100), phoneNumber VARCHAR(15), password VARCHAR(100))"""
+                sql = f""" CREATE TABLE {table_name}(id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY, username VARCHAR(100), emailAddress VARCHAR(100), phoneNumber VARCHAR(15), password VARCHAR(100))"""
 
                 cursor.execute(sql)
                 connection.commit()
         except:
                 print('Table already exists')
 
-def add(user_profile):
+def add(user):
 
     with connection.cursor() as cursor:
 
-        sql = """ INSERT INTO User(username, emailAddress, phoneNumber, password) VALUES('{}', '{}', '{}', '{}')""".format(user_profile[0], user_profile[1], user_profile[2], user_profile[3])
+        sql = """ INSERT INTO user(username, emailAddress, phoneNumber, password) VALUES('{}', '{}', '{}', '{}')""".format(user.username, user.email, user.phone, user.password)
 
         cursor.execute(sql)
         connection.commit()
@@ -35,3 +37,17 @@ def retrieve_for_authentication(username, password):
                 except:
                         print('The username or password not found')
                 
+
+def check(user):
+
+        with connection.cursor() as cursor:
+
+                try:
+                        sql = """ SELECT username FROM {} WHERE username = '{}'""".format(table_name, user.username)
+                        cursor.execute(sql)
+
+                        return len(cursor.fetchone()) > 0
+                except:
+                        print("Table does not exist")
+                        return False
+                        
